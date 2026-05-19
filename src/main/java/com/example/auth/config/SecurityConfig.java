d113165da546f9f8d80fabfe493a16d5cb29c779
deepseem <deepakmahto3039@gmail.com>
2026-05-19 10:40:47 +0530

    refsctor first commit-2

--- src/main/java/com/example/auth/config/SecurityConfig.java
+++ src/main/java/com/example/auth/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
